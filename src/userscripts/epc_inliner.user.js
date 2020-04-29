babda04f4cc6426a6760d1f5eed6db07cafc6b26
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-29 21:27:36 +0100

    Display EPCs on results pages

--- src/userscripts/epc_inliner.user.js
+++ src/userscripts/epc_inliner.user.js
@@ -1,22 +1,60 @@
 // ==UserScript==
 // @name         Energy Performance Certificate (EPC) inliner
 // @namespace    https://tridactyl.xyz
-// @version      0.1
+// @version      0.2
 // @description  Inlines EPC images on estate agent listing pages. Currently only works on Zoopla.
 // @author       bovine3dom
 // @match        https://www.zoopla.co.uk/to-rent/details/*
 // @match        https://www.zoopla.co.uk/for-sale/details/*
+// @match        https://www.zoopla.co.uk/to-rent/property/*
+// @match        https://www.zoopla.co.uk/for-sale/property/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    'use strict'
+    const url = document.location.href
+    if (url.match(/zoopla\.co\.uk\/(to-rent|for-sale)/) !== null) {
+        if (url.match(/property/) !== null){
+            for (let x of document.querySelectorAll("a.listing-results-price.text-price")){
+                epc_nabber_element(x).then(r=> {
+                    try {
+                        x.parentElement.parentElement.querySelector("div.status-wrapper").children[1].appendChild(r)
+                    } catch(e) {
+                        x.parentElement.appendChild(r)
+                    }
+                })
+            }
+        } else if (url.match(/details/) !== null){
+            document.getElementById("property-details-tab").children[1].appendChild(page2epc_element(document))
+        }
+    } // logic for other sites if we decide to support them goes here
+})()
+
+function page2epc_element(page){
     try {
-        const url = document.getElementById("epc-1").getElementsByClassName("ui-modal-gallery__asset ui-modal-gallery__asset--center-content")[0].style.backgroundImage.slice(5,-2);
-        const img = document.createElement("img"); img.src = url;
-        document.getElementById("property-details-tab").children[1].appendChild(img);
+        const url = page2epc_url(page)
+        const img = document.createElement("img")
+        img.src = url
+        img.style.maxWidth = "100%"
+        return img
     } catch(e) {
-        const div = document.createElement("div"); div.innerHTML = "<b>NB:</b> No EPC found for this property. Expect the worst.";
-        document.getElementById("property-details-tab").children[1].appendChild(div);
+        const div = document.createElement("div")
+        div.innerHTML = "<b>NB:</b> No EPC found for this property. Expect the worst."
+        return div
     }
-})();
+}
+
+function page2epc_url(page){
+    return page.querySelector("#epc-1").getElementsByClassName(
+        "ui-modal-gallery__asset ui-modal-gallery__asset--center-content"
+    )[0].style.backgroundImage.slice(5,-2)
+}
+
+async function epc_nabber_element(link){
+    const page = await (await fetch(link)).text()
+    const dummy = document.createElement('html')
+    dummy.innerHTML = page
+    return page2epc_element(dummy)
+}
+
