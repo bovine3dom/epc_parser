c65e0ef1749c26647fe22aa8f54e71d5f4754471
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-30 13:44:46 +0100

    Refactor to reduce duplication

--- src/webextension/src/epc_inliner.user.js
+++ src/webextension/src/epc_inliner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Energy Performance Certificate (EPC) inliner
 // @namespace    https://tridactyl.xyz
-// @version      0.0.2
+// @version      0.0.3
 // @description  Inlines EPC images on estate agent listing pages for Zoopla and Rightmove.
 // @author       bovine3dom
 // @match        https://www.zoopla.co.uk/*
@@ -15,7 +15,8 @@
     if (url.match(/zoopla\.co\.uk\/(to-rent|for-sale|new-homes)/) !== null) {
         if (url.match(/property/) !== null){
             for (let x of document.querySelectorAll("a.listing-results-price.text-price, a.lsrp-property-card__pricing-link")){
-                epc_nabber_element(x).then(r=> {
+                fake_page(x).then(r=> {
+                    r = zp_page2epc_element(r)
                     try {
                         x.parentElement.parentElement.querySelector("div.status-wrapper").children[1].appendChild(r)
                     } catch(e) {
@@ -24,18 +25,17 @@
                 })
             }
         } else if (url.match(/details/) !== null){
-            document.getElementById("property-details-tab").children[1].appendChild(page2epc_element(document))
+            document.getElementById("property-details-tab").children[1].appendChild(zp_page2epc_element(document))
         }
     } else if (url.match(/rightmove\.co\.uk\/property-(to-rent|for-sale)/) !== null) {
         if (url.match(/property-[0-9]*\.html/) !== null){
             rm_page2epc_url(document).then(src => {
-                document.querySelector("div.agent-content").children[0].appendChild(rm_url2epc_element(src))
+                document.querySelector("div.agent-content").children[0].appendChild(img(src))
             })
         } else {
             for (let x of document.querySelectorAll("a.propertyCard-link[data-test='property-details']")){
                 rm_epc_nabber_element(x.href).then(i=>{
-                    console.log(i);
-                    i.style.maxWidth = "50%";
+                    i.style.maxWidth = "50%"
                     x.parentNode.parentNode.parentNode.parentNode.appendChild(i)
                 })
             }
@@ -43,60 +43,55 @@
     }
 })()
 
+// Generic
+function error_div(){
+    const div = document.createElement("div")
+    div.innerHTML = "<b>NB:</b> No EPC found for this property."
+    return div
+}
+
+async function fake_page(link){
+    const page = await (await fetch(link)).text()
+    const dummy = document.createElement('html')
+    dummy.innerHTML = page
+    return dummy
+}
+
+function img(link){
+    const img = document.createElement("img")
+    img.src = link
+    img.style.maxWidth = "100%"
+    return img
+}
+
 // Zoopla
-function page2epc_element(page){
+function zp_page2epc_element(page){
     try {
-        const url = page2epc_url(page)
-        const img = document.createElement("img")
-        img.src = url
-        img.style.maxWidth = "100%"
-        return img
+        const url = zp_page2epc_url(page)
+        return img(url)
     } catch(e) {
-        const div = document.createElement("div")
-        div.innerHTML = "<b>NB:</b> No EPC found for this property."
-        return div
+        return error_div()
     }
 }
 
-function page2epc_url(page){
+function zp_page2epc_url(page){
     return page.querySelector("#epc-1").getElementsByClassName(
         "ui-modal-gallery__asset ui-modal-gallery__asset--center-content"
     )[0].style.backgroundImage.slice(5,-2)
 }
 
-async function epc_nabber_element(link){
-    const page = await (await fetch(link)).text()
-    const dummy = document.createElement('html')
-    dummy.innerHTML = page
-    return page2epc_element(dummy)
-}
-
 // Rightmove
 async function rm_epc_nabber_element(link){
     try {
-        const page = await (await fetch(link)).text()
-        const dummy = document.createElement('html')
-        dummy.innerHTML = page
-        const url = await rm_page2epc_url(dummy)
-        return rm_url2epc_element(url)
+        const url = await rm_page2epc_url(await fake_page(link))
+        return img(url)
     } catch(e) {
-        const div = document.createElement("div")
-        div.innerHTML = "<b>NB:</b> No EPC found for this property."
-        return div
+        return error_div()
     }
 }
 
-function rm_url2epc_element(url){
-        const img = document.createElement("img")
-        img.src = url
-        img.style.maxWidth = "100%"
-        img.style.maxHeight = "100%"
-        return img
-}
 async function rm_page2epc_url(page){
     const redir_link = document.evaluate("//h3[contains(., 'Energy Performance Certificate (EPC) graphs')]", page, null, XPathResult.ANY_TYPE, null ).iterateNext().parentNode.querySelector("a").href
-    const redir = await (await fetch(redir_link)).text()
-    const dummy = document.createElement('html')
-    dummy.innerHTML = redir
+    const dummy = await fake_page(redir_link)
     return dummy.querySelector("a").href
 }
