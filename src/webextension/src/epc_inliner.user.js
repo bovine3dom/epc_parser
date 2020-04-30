94a85909e9e9ad03c06debf09da7623db5c64b5d
Oliver Blanthorn <freedom4cows@gmail.com>
2020-04-30 13:31:00 +0100

    Add rightmove support
    Need refactor to reduce code duplication

--- src/webextension/src/epc_inliner.user.js
+++ src/webextension/src/epc_inliner.user.js
@@ -2,14 +2,10 @@
 // @name         Energy Performance Certificate (EPC) inliner
 // @namespace    https://tridactyl.xyz
 // @version      0.0.2
-// @description  Inlines EPC images on estate agent listing pages. Currently only works on Zoopla.
+// @description  Inlines EPC images on estate agent listing pages for Zoopla and Rightmove.
 // @author       bovine3dom
-// @match        https://www.zoopla.co.uk/to-rent/details/*
-// @match        https://www.zoopla.co.uk/for-sale/details/*
-// @match        https://www.zoopla.co.uk/new-homes/details/*
-// @match        https://www.zoopla.co.uk/to-rent/property/*
-// @match        https://www.zoopla.co.uk/for-sale/property/*
-// @match        https://www.zoopla.co.uk/new-homes/property/*
+// @match        https://www.zoopla.co.uk/*
+// @match        https://www.rightmove.co.uk/*
 // @grant        none
 // ==/UserScript==
 
@@ -30,9 +26,24 @@
         } else if (url.match(/details/) !== null){
             document.getElementById("property-details-tab").children[1].appendChild(page2epc_element(document))
         }
-    } // logic for other sites if we decide to support them goes here
+    } else if (url.match(/rightmove\.co\.uk\/property-(to-rent|for-sale)/) !== null) {
+        if (url.match(/property-[0-9]*\.html/) !== null){
+            rm_page2epc_url(document).then(src => {
+                document.querySelector("div.agent-content").children[0].appendChild(rm_url2epc_element(src))
+            })
+        } else {
+            for (let x of document.querySelectorAll("a.propertyCard-link[data-test='property-details']")){
+                rm_epc_nabber_element(x.href).then(i=>{
+                    console.log(i);
+                    i.style.maxWidth = "50%";
+                    x.parentNode.parentNode.parentNode.parentNode.appendChild(i)
+                })
+            }
+        }
+    }
 })()
 
+// Zoopla
 function page2epc_element(page){
     try {
         const url = page2epc_url(page)
@@ -60,3 +71,32 @@ async function epc_nabber_element(link){
     return page2epc_element(dummy)
 }
 
+// Rightmove
+async function rm_epc_nabber_element(link){
+    try {
+        const page = await (await fetch(link)).text()
+        const dummy = document.createElement('html')
+        dummy.innerHTML = page
+        const url = await rm_page2epc_url(dummy)
+        return rm_url2epc_element(url)
+    } catch(e) {
+        const div = document.createElement("div")
+        div.innerHTML = "<b>NB:</b> No EPC found for this property."
+        return div
+    }
+}
+
+function rm_url2epc_element(url){
+        const img = document.createElement("img")
+        img.src = url
+        img.style.maxWidth = "100%"
+        img.style.maxHeight = "100%"
+        return img
+}
+async function rm_page2epc_url(page){
+    const redir_link = document.evaluate("//h3[contains(., 'Energy Performance Certificate (EPC) graphs')]", page, null, XPathResult.ANY_TYPE, null ).iterateNext().parentNode.querySelector("a").href
+    const redir = await (await fetch(redir_link)).text()
+    const dummy = document.createElement('html')
+    dummy.innerHTML = redir
+    return dummy.querySelector("a").href
+}
